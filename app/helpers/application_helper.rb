aa1809ca41937b3aa9828f5ffebfde0fa4482a0f
Milly <plmrv2000@gmail.com>
2025-11-01 15:16:54 +0300

    fix 4 lab

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,4 @@
+# app/helpers/application_helper.rb
 module ApplicationHelper
   def full_title(page_title = '')
     base_title = "Ruby on Rails Tutorial Sample App"
@@ -7,5 +8,11 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
-end
 
+  # Новый хелпер: возвращает строку с длиной заголовка
+  def title_with_length(page_title = '')
+    title = full_title(page_title)
+    length = title.length
+    "#{title} — #{length} символов"
+  end
+end
